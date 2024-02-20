e6aee08e3f949bbdae6aa195c976b65ab59c5cb8
nitroEef <mikebodetelli@gmail.com>
2024-02-20 14:42:50 +0100

    update

---
+++ stringanditsmethods/script.js
@@ -0,0 +1,24 @@
+// types of String
+
+// single quote ('')
+
+const world = 'i am a boy'
+
+
+//double quote("")
+const words = "i am a boy"
+
+// backticks (``)
+const worlds = `i am a boy`
+
+console.log(world, words, worlds)
+
+
+const age = 12
+const school = 'DLT'
+const name = 'eef'
+
+const sentence =`my name is ${name}, i am ${age} years old and i go to ${school} school`
+console.log(sentence)
+
+const math = `2+2`
--- trickyconcept/script.js
+++ trickyconcept/script.js
@@ -65,19 +65,25 @@ const exampleThree = () => {
     }
 }
 
+
+//block code
+// when u declare a variable inside the CSSLayerBlockRule, it can not be acessed outside the code
+
+
+
+
+
+
+
+//  HOISTING
 // Hoisting is a concept that enables us to extract values of variables and functions even before initializing/assigning value without getting errors and this happens during the 1st phase (memory creation phase) of the Execution Context.
 
 // Features of Hoisting:
 // In JavaScript, Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution. Basically, it gives us an advantage that no matter where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or local. 
 // It allows us to call functions before even writing them in our code. 
 // Note: JavaScript only hoists declarations, not initializations.
-
 // JavaScript allocates memory for all variables and functions defined in the program before execution.
 
-
-
-
-//  HOISTING
 
 const a = 2
 const b = 3
@@ -87,4 +93,48 @@ console.log(result)
 
 function exampleFive() {
     return 2 + 2
+}
+
+
+//C L O S U R E 
+// the scope regarding a function inside a function
+
+const outerFunction = () => {
+    const ben = 5
+
+
+const innerFunction = () => {
+    const man = 7
+    return ben +  man
+} 
+    const final = innerFunction()
+    console.log(final + 2)
+}
+
+outerFunction()
+
+//exercise that carries
+//  a function that carries a function with the condition that if time return a certain number then
+// return the final answer of the condition *4
+// and in the main function the final answer of the previous question 
+// should be divided by 2 and i wnt asnwer in console.
+
+
+const firstFunction = () => {
+    const div = 2
+
+    const secondFunction = () =>{
+        const condition = true
+        let certainNumber
+        const mult = 4
+
+        if(condition){
+            certainNumber = 5
+        }
+        const ans = certainNumber + mult
+        return ans
+    }
+    const initialAnswer = secondFunction()
+    const finalAnswer = initialAnswer / div
+    console.log (finalAnswer)
 }
