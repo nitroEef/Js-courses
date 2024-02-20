aa52d4df847914c0bd264b0eeac5a295733eb630
nitroEef <mikebodetelli@gmail.com>
2024-02-20 15:08:33 +0100

    update

--- stringanditsmethods/script.js
+++ stringanditsmethods/script.js
@@ -1,24 +1,46 @@
-// types of String
+// // types of String
 
-// single quote ('')
+// // single quote ('')
 
-const world = 'i am a boy'
-
-
-//double quote("")
-const words = "i am a boy"
-
-// backticks (``)
-const worlds = `i am a boy`
-
-console.log(world, words, worlds)
+// const world = 'i am a boy'
 
 
-const age = 12
-const school = 'DLT'
-const name = 'eef'
+// //double quote("")
+// const words = "i am a boy"
 
-const sentence =`my name is ${name}, i am ${age} years old and i go to ${school} school`
-console.log(sentence)
+// // backticks (``)
+// const worlds = `i am a boy`
 
-const math = `2+2`
+// console.log(world, words, worlds)
+
+
+// const age = 12
+// const school = 'DLT'
+// const name = 'eef'
+
+// const sentence =`my name is ${name}, i am ${age} years old and i go to ${school} school`
+// console.log(sentence)
+
+// const math = `2+2`
+
+// const mathematics = `${2+2}`
+// console.log(mathematics)
+
+// const lines = `1
+// 2
+// 3
+// 4
+// 5
+// `
+// console.log(lines)
+
+
+// const numerals = `hello i'm Aliyu and i am from "Kogi state"`
+// console.log(numerals)
+
+//to check how many characters in a string 
+
+const name = "hayzed"
+console.log(name.length)
+
+console.log(name[0])
