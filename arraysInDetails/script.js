b736448f8ed8a3a3ac7527cd913842d654fbaa9b
nitroEef <mikebodetelli@gmail.com>
2024-02-21 12:24:55 +0100

    update

---
+++ arraysInDetails/script.js
@@ -0,0 +1,39 @@
+const cohort = ["soliu",
+"mufu", 
+"tobi",
+ "seyi",
+ "dele",
+ "dad",
+];
+console.log(cohort);
+
+
+// my own
+// for (let i = 0; i < cohort.length; i++) {
+// console.log(cohort[i])}
+
+// tutors own 
+for (let i = 0; i < cohort.length; i++) {
+     console.log(i,cohort[i])}
+
+
+// /example
+
+const nested = [[1,2], [3,4] [5,6]]
+const newArr = nested[0]
+console.log(newArr);
+
+const object ={
+    name :"john",
+    age : 60
+}
+console.log(object)
+
+
+
+//example
+
+const arr = [
+    "banana",
+    10000,
+]
