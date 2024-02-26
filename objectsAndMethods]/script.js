013d68d907580826eacc0fcf0dd85c2c2780d757
nitroEef <mikebodetelli@gmail.com>
2024-02-26 15:35:01 +0100

    update

--- objectsAndMethods]/script.js
+++ objectsAndMethods]/script.js
@@ -20,11 +20,28 @@ console.log(person)
 
 //assessing and adding of properties to an object
 const human = {
-    firstName : 'Abdullah' 
+    firstName : 'Abdullah',
+    "DLT africa location":"pegamut" 
 }
 
 human.pet = {name:"cobra", age:3}
 human.job = "headmaster"
 human.favFood = "garri"
+
 console.log(human)
 console.log(human.firstName)
+console.log(human['firstName']) 
+console.log(human['DLT africa location'])
+
+//another way to call it out
+console.log(human.pet.name)
+
+
+//object built in method
+const pet = {
+    petName : "dog",
+    petSound:() => {
+        console.log("woof woof")
+    }
+}
+pet.petSound()
