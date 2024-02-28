a4fc4d41d799e501ac14ade64220c71b85a0fd1a
nitroEef <mikebodetelli@gmail.com>
2024-02-28 13:27:34 +0100

    update

--- dom example/script.js
+++ dom example/script.js
@@ -1,1 +1,10 @@
-const anything = document.querySelectorAll
+const anything = document.querySelectorAll('.menuItem');
+const oneEl = document.querySelector('.active')
+
+const menuClicked = (curEl) => {
+    for (let i = 0; i < anything.length; i++){
+        anything[i].classList.remove('active')
+    }
+    curEl.classList.add('active')
+}
+
