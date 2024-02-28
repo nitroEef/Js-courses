622e821035a19e5f3f31c2faa296195fa07b8d39
nitroEef <mikebodetelli@gmail.com>
2024-02-28 12:13:38 +0100

    update

--- Cloning/cloning.js
+++ Cloning/cloning.js
@@ -24,51 +24,51 @@ console.log(otherPerson)
 // const newNumber = [...numbers];
 // console.log(newNumber);
 
-// to get the equality 
-const numbers =  [1,2,3,4,5,6];
-const copiedNumber = numbers;
-const newNumber = [...numbers];
-const numbers$ = numbers.slice(2,4)
+// // to get the equality 
+// const numbers =  [1,2,3,4,5,6];
+// const copiedNumber = numbers;
+// const newNumber = [...numbers];
+// const numbers$ = numbers.slice(2,4)
 
-console.log(copiedNumber);
-console.log(numbers===copiedNumber);
-console.log(numbers===newNumber);
+// console.log(copiedNumber);
+// console.log(numbers===copiedNumber);
+// console.log(numbers===newNumber);
 
-numbers.push(12);
-console.log(numbers)
-console.log(numbers$)
+// numbers.push(12);
+// console.log(numbers)
+// console.log(numbers$)
 
-console.log(copiedNumber)
-console.log(newNumber)
+// console.log(copiedNumber)
+// console.log(newNumber)
 
 
-//object cloning
+// //object cloning
 
-const personDetails = {
-    name : "zainab",
-    age : 40 
-}
-const changeDetails = personDetails;
-personDetails.age = 10
-console.log(personDetails)
-console.log(changeDetails)
+// const personDetails = {
+//     name : "zainab",
+//     age : 40 
+// }
+// const changeDetails = personDetails;
+// personDetails.age = 10
+// console.log(personDetails)
+// console.log(changeDetails)
 
 //shallow clone using assign object
-const originalObject = {
-    name : "Bash",
-    age : 20,
-    address : {
-        city : "pegamut",
-        state : "Ogun "
-    }
-};
+// const originalObject = {
+//     name : "Bash",
+//     age : 20,
+//     address : {
+//         city : "pegamut",
+//         state : "Ogun "
+//     }
+// };
 
-const cloneObj = Object.assign({}, originalObject);
-cloneObj.name = "Naphy";
-cloneObj.address.city = "Elejigbo"
+// const cloneObj = Object.assign({}, originalObject);
+// cloneObj.name = "Naphy";
+// cloneObj.address.city = "Elejigbo"
 
-console.log(originalObject)
-console.log(cloneObj)
+// console.log(originalObject)
+// console.log(cloneObj)
 
 
 
@@ -97,6 +97,27 @@ console.log(cloneMerged)
 // new object do not affect the original and vice versa. this will creste a new object but
 //  nested object will still be reference to the original one
 
+// const details = {
+//     firstName : "kenny",
+//     car : {
+//         brand : "toyota",
+//         wheels : 4,
+//         color : "red"
+//     }
+// }
+
+// const  newDetails = {...details}
+// newDetails.firstName = "mike",
+// newDetails.car.color = "blue"
+
+
+// console.log(details);
+// console.log(newDetails)
+
+
+
+// the json.stringify method convert a js object or any format
+//  to string and during this process all the reference are destroyed 
 const details = {
     firstName : "kenny",
     car : {
@@ -105,9 +126,32 @@ const details = {
         color : "red"
     }
 }
+const stringifiedDetails = JSON.stringify(details);
+console.log(stringifiedDetails);
 
-const  newDetails = {...details}
-newDetails.firstName = "mike",
-console.log(details);
-console.log(newDetails)
+const newDetails = JSON.parse(stringifiedDetails);
+console.log(newDetails);
+
+newDetails.car.color = "blue"
+console.log(details.car.color)
+console.log(newDetails.car.color)
+
+
+const originalObj ={
+    name : "zainab",
+    age : 12,
+    address : {
+        local :"agege wa",
+        area: 'baba yemi'
+    }
+}
+
+const cloneObj = JSON.parse(JSON.stringify(originalObj));
+cloneObj.name = 'soliu'
+cloneObj.age = 54
+cloneObj.address.local = 'pegamut'
+cloneObj.address.area = "mujayyidun"
+
+console.log(originalObj)
+console.log(cloneObj)
 
