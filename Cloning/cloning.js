994c804421ad8ea75c07ba16c4c366fdcc533559
nitroEef <mikebodetelli@gmail.com>
2024-02-28 10:42:34 +0100

    update

--- Cloning/cloning.js
+++ Cloning/cloning.js
@@ -14,6 +14,58 @@ console.log(otherPerson)
 //  or array, the shallow cloe will create new reference to those 
 //  nested object rather than creating new copies. when u have nested object inside an object
 //  the reference here will tamper the child object instead of the Main object
+//spread operator(...);
 
-const numbers = [1,2,3,4,5,6];
-const newNumber = 
+//array clone
+
+// const numbers = [1,2,3,4,5,6];
+// console.log(...numbers);
+
+// const newNumber = [...numbers];
+// console.log(newNumber);
+
+// to get the equality 
+const numbers =  [1,2,3,4,5,6];
+const copiedNumber = numbers;
+const newNumber = [...numbers];
+const numbers$ = numbers.slice(2,4)
+
+console.log(copiedNumber);
+console.log(numbers===copiedNumber);
+console.log(numbers===newNumber);
+
+numbers.push(12);
+console.log(numbers)
+console.log(numbers$)
+
+console.log(copiedNumber)
+console.log(newNumber)
+
+
+//object cloning
+
+const personDetails = {
+    name : "zainab",
+    age : 40 
+}
+const changeDetails = personDetails;
+personDetails.age = 10
+console.log(personDetails)
+console.log(changeDetails)
+
+//shallow clone using assign object
+const originalObject = {
+    name : "Bash",
+    age : 20,
+    address : {
+        city : "pegamut",
+        state : "Ogun "
+    }
+}
+
+
+
+
+
+
+
