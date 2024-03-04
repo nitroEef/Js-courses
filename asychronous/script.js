3b3be5538c531c3b997b00e8a646209bab23dd25
nitroEef <mikebodetelli@gmail.com>
2024-03-04 09:33:47 +0100

    updte

---
+++ asychronous/script.js
@@ -0,0 +1,8 @@
+//when we are talking aabout asy, we are talking about setinterval() and settimeout()
+//method dey with parenthesis
+// interval allow u to execute a chunk of codes
+//  everytime a specified amount of milliseconds passes
+
+// settimeout functions allow u to wait a certain amount
+//  of time before a executing a chunk of codes, do note other codes outside of timeout will 
+//  continue execution as normal 
