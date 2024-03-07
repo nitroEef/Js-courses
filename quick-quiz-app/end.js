6304b8d8d717ef1988e41371cc9b0f9640946bfc
nitroEef <mikebodetelli@gmail.com>
2024-03-07 12:47:22 +0100

    update

--- quick-quiz-app/end.js
+++ quick-quiz-app/end.js
@@ -6,3 +6,11 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 console.log(mostRecentScore)
 finalScore.innerText = mostRecentScore;  //to get out score in the local storage
+
+// our button should be clickable anytime we type the username 
+// we have the save button disabled by defaault. 
+// keyup means when we type something into the input box, it will check if there is something in the input box, it should be enabled
+// when we click on username..keyup is one of the event
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.value;
+})
