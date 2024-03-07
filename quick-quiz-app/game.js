b78c41a4847b04d26943e453d6e14dbaaed6c432
nitroEef <mikebodetelli@gmail.com>
2024-03-07 09:59:58 +0100

    update

--- quick-quiz-app/game.js
+++ quick-quiz-app/game.js
@@ -1,20 +1,22 @@
-// get elements with dom ... create an array of questions 
-const question = document.getElementById('question')
-const progressText= document.getElementById('progressText')
-const scoreText= document.getElementById('score')
-const progressBarFull= document.getElementById('progressBarFull')
-const games= document.getElementById('game')
-const choiceText = Array.from(document.getElementsByClassName('choice-text'))
 
-let currentQuestion = {} ;
-let acceptingAnswer = false ;
-let score = 0 ;
-let questionCounter = 0 ;
-let availableQuestion = [] ;
+
+// Get Element with DOM --> create an array of question.
+const question = document.getElementById("question");
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("Score");
+const progressBarFull = document.getElementById("progressBarFull");
+const game = document.getElementById("game");
+const choice = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
 
 let questions = [
     {
-        "question": "Inside which HTML element do we put the JavaScript??",
+        "question": "Inside which HTML element do we put the JavaScript?",
         "choice1": "<script>",
         "choice2": "<javascript>",
         "choice3": "<js>",
@@ -35,33 +37,53 @@ let questions = [
         "choice2": "alertBox('Hello World');",
         "choice3": "msg('Hello World');",
         "choice4": "alert('Hello World');",
-        "answer": 4
-      }
-
+        "answer": 4
+      }
 ]
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3 ;
+const MAX_QUESTION = 3;
 
 //START QUIZ
 
-startGame = () => {
-    questionCounter=0;
+const startGame = () => {
+    questionCounter = 0;
     score = 0;
-    availableQuestion = [...questions] //shalow cloning, copying questions to availablequestion
-    console.log(availableQuestion)
+    availableQuestion = [...questions];
+    console.log(availableQuestion);
     getNewQuestion()
 }
 
-// const getNewQuestion = () => {
-//     questionCounter++;
-//     progressText.innerText=`Question ${questionCounter}/${MAX_QUESTIONS}`;
-//     progressBarFull.style.width = '${(questionCounter / MAX_QUESTIONS)'
-// }
 const getNewQuestion = () => {
     questionCounter++;
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = $`{(questionCounter / MAX_QUESTIONS) * 100}%`;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTION} `;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTION) * 100}%`
+
+    const questionIndex = Math.floor(Math.random() * availableQuestion.length);
+    currentQuestion = availableQuestion[questionIndex];
+    question.innerText = currentQuestion.question
+    // console.log(currentQuestion);
+
+
+    choice.forEach((choice) => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number]
+    })
+availableQuestion.splice(questionIndex,1);
+acceptingAnswer=true
 }
 
-startGame()
+// always use preventdefault anytime u add addEventListener, 
+// cos if it is clicked anytime u add addevent it wil reload the page
+// so preventdefault wont all the page to be reloaded when clicked
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if(!acceptingAnswer) return;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"]
+    })
+})
+startGame();
