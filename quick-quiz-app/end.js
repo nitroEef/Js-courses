da1ef095a608fcab0555cd0bdc1be420b5c45b7f
nitroEef <mikebodetelli@gmail.com>
2024-03-07 15:22:48 +0100

    update

--- quick-quiz-app/end.js
+++ quick-quiz-app/end.js
@@ -1,44 +1,44 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores =JSON.parse(localStorage.getItem("highScores"))||[]
-console.log(mostRecentScore)
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(mostRecentScore);
 
-// we want to specify how many highscore do we wnt to display on our highscore page 
-const MAX_HIGH_SCORE = 5
+// we want to specify how many highscore do we wnt to display on our highscore page
+const MAX_HIGH_SCORE = 5;
 
-finalScore.innerText = mostRecentScore;  //to get out score in the local storage
+finalScore.innerText = mostRecentScore; //to get out score in the local storage
 
-// our button should be clickable anytime we type the username 
-// we have the save button disabled by defaault. 
+// our button should be clickable anytime we type the username
+// we have the save button disabled by defaault.
 // keyup means when we type something into the input box, it will check if there is something in the input box, it should be enabled
 // when we click on username..keyup is one of the event
 username.addEventListener("keyup", () => {
-    saveScoreBtn.disabled = !username.value;
+  saveScoreBtn.disabled = !username.value;
 });
 
-// to store the name of the player and his or her score in the local storage 
+// to store the name of the player and his or her score in the local storage
 // after saving the highsvore. it should redirect to the homepage
 const saveHighScore = () => {
-    const score = {
-        score: mostRecentScore,
-        name : username.value
-    }
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
 
-    // to push the score inside the highscore array 
-    highScores.push(score)
-    highScores.sort((a,b) => b.score - a.score);
-    highScores.splice(5)
+  // to push the score inside the highscore array
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
 
-    console.log(highScores)
+  console.log(highScores);
 
-    localStorage.setItem('highScore', JSOn.stringify(highScores));
+  localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign("./index.html")
 }
 
-saveScoreBtn.addEventListener('click',(event) => {
-    event.preventDefault();
-    saveHighScore()
-})
+saveScoreBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  saveHighScore();
+});
