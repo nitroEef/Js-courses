a4415446d54a3d2154d70310ba3de7ca656fcb6f
nitroEef <mikebodetelli@gmail.com>
2024-03-11 14:12:38 +0100

    update

--- SPOCK-ROCK/script.js
+++ SPOCK-ROCK/script.js
@@ -2,8 +2,8 @@
 
 const playerScoreEl = document.getElementById("playerScore");
 const playerChoiceEl = document.getElementById("playerChoice");
-const computerScoreE1 = document.getElementById("computerScpore");
-const computerChoice = document.getElementById("computerChoice");
+const computerScoreE1 = document.getElementById("computerScore");
+const computerChoiceEl = document.getElementById("computerChoice");
 const resultText = document.getElementById("resultText");
 
 const playerRock = document.getElementById("playerRock");
@@ -34,15 +34,80 @@ const choice = {
 // //     console.log(playerChoice)
 // // }
 
+let computerChoice = "";
+
 // reset all selected icons 
 function resetSelected(){
     allGameIcon.forEach((icon) => {
         icon.classList.remove('selected')
     })
 }
+//random computer choice
+function computerRandomChoice(){
+    const computerChoiceNumber = Math.random();
+    if(computerChoiceNumber < 0.2){
+        computerChoice = 'rock'
+    }else if(computerChoiceNumber <= 0.4){
+        computerChoice = 'paper'
+    }
+    else if(computerChoiceNumber <= 0.6){
+        computerChoice = 'scissors'
+    }
+    else if(computerChoiceNumber <= 0.8){
+    computerChoice = 'lizard'
+    }
+    else {
+        computerChoice = 'spock'
+    }
+    console.log(computerChoice)
+}
+
+
+
+
+
+
+//call functions to process turn
+//the function that maks sure when clicked, 
+// reset selected and computer randsom complete teir task 
+function checkResult(){
+    resetSelected();
+    computerRandomChoice()
+}
 
 function select(playerChoice){
-    resetSelected();
+    checkResult();
+
+
+    switch(playerChoice){
+        case 'roch':
+            playerRock.classList.add('selected');
+            playerChoiceEl.textContent ='---Rock'
+            break;
+        case 'paper':
+            playerPaper.classList.add('selected');
+            playerChoiceEl.textContent ='---Paper'
+            break;
+        case 'scissors':
+            playerScissors.classList.add('selected');
+            playerChoiceEl.textContent ='---Scissors'
+            break;
+        case 'lizard':
+            playerLizard.classList.add('selected');
+            playerChoiceEl.textContent ='---Lizard'
+            break;
+        case 'spock':
+            playerSpock.classList.add('selected');
+            playerChoiceEl.textContent ='---Spock'
+            break;
+        
+    }
+}
+
+
+
+function displayComputerChoice(playerChoice){
+    checkResult();
 
 
     switch(playerChoice){
