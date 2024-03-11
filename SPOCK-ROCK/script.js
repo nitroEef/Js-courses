9ad182a86137e55781d6f0bfbc02269e411cf2bd
nitroEef <mikebodetelli@gmail.com>
2024-03-11 12:40:01 +0100

    update

--- SPOCK-ROCK/script.js
+++ SPOCK-ROCK/script.js
@@ -1,7 +1,71 @@
-const choices = {
-    rock: {name: "Rock", defeats:['scissors','lizard']},
-    paper: {name: "Paper", defeats:['rock','spock']},
-    scissors: {name: "Scissors", defeats:['paper','lizard']},
-    lizard: {name: "Lizard", defeats:['paper','spock']},
-    spock: {name: "Spock", defeats:['scissors','rock']}
+
+
+const playerScoreEl = document.getElementById("playerScore");
+const playerChoiceEl = document.getElementById("playerChoice");
+const computerScoreE1 = document.getElementById("computerScpore");
+const computerChoice = document.getElementById("computerChoice");
+const resultText = document.getElementById("resultText");
+
+const playerRock = document.getElementById("playerRock");
+const playerPaper = document.getElementById("playerPaper");
+const playerSpock = document.getElementById("playerSpock");
+const playerScissors= document.getElementById("playerScissors");
+const playerLizard= document.getElementById("playerLizard");
+
+const computerRock = document.getElementById("computerRock");
+const computerPaper = document.getElementById("computerPaper");
+const computerSpock = document.getElementById("computerSpock");
+const computerScissors = document.getElementById("computerScissors");
+const computerLizard = document.getElementById("computerLizard");
+
+const allGameIcon = document.querySelectorAll(".far")
+
+
+const choice = {
+    rock: {name: "rock", defeats: ["scissors", "lizard"]},
+    rock: {name: "paper", defeats: ["rocks", "spock"]},
+    rock: {name: "scissors", defeats: ["paper", "lizard"]},
+    rock: {name: "lizard", defeats: ["paper", "spock"]},
+    rock: {name: "rock", defeats: ["scissors", "rock"]},
+}
+
+// //this will select all the choices in the console while selected
+// // function select(playerChoice){
+// //     console.log(playerChoice)
+// // }
+
+// reset all selected icons 
+function resetSelected(){
+    allGameIcon.forEach((icon) => {
+        icon.classList.remove('selected')
+    })
+}
+
+function select(playerChoice){
+    resetSelected();
+
+
+    switch(playerChoice){
+        case 'roch':
+            playerRock.classList.add('selected');
+            playerChoiceEl.textContent ='---Rock'
+            break;
+        case 'paper':
+            playerPaper.classList.add('selected');
+            playerChoiceEl.textContent ='---Paper'
+            break;
+        case 'scissors':
+            playerScissors.classList.add('selected');
+            playerChoiceEl.textContent ='---Scissors'
+            break;
+        case 'lizard':
+            playerLizard.classList.add('selected');
+            playerChoiceEl.textContent ='---Lizard'
+            break;
+        case 'spock':
+            playerSpock.classList.add('selected');
+            playerChoiceEl.textContent ='---Spock'
+            break;
+        
+    }
 }
